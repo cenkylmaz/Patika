b36d0a8328ac20b2bfd9107175493c9637866b7e
Cenkylmaz <151631495+cenkylmaz@users.noreply.github.com>
2025-03-18 19:38:11 +0300

    C commit

---
+++ PatikaÖdev/hafta.1/Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int number = 5;
+        int number2 = 10;
+        int number3 = 10;
+
+        int [] numbers = new int[5];
+        numbers[0] = number;
+        numbers[1] = number2;
+
+
+
+}
+}
