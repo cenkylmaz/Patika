a054a8b3df2609fc490f91ded3e424485f3ba587
Cenkylmaz <151631495+cenkylmaz@users.noreply.github.com>
2025-03-20 14:06:47 +0300

    c commit

---
+++ untitled1/src/ArrayTest.java
@@ -0,0 +1,16 @@
+public class ArrayTest {
+    public static void main(String[] args) {
+        int[] sayilara = {1,2,4,50};
+        System.out.println(sayilara[1]); // dizilere bakmak için kapalı parantez kullanıyoruz . ([])
+        System.out.println(sayilara[2]);
+     int[] // array tanımı
+        int[] numbers = {1,2,3,4,5};
+      int [][] // multi arrays
+
+
+
+
+
+
+}
+}
