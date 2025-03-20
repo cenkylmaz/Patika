0f410cc85e8a3bd422815f438e0d923e4cb6c739
Cenkylmaz <151631495+cenkylmaz@users.noreply.github.com>
2025-03-20 14:12:55 +0300

    Update TersÜçgen.java

--- PatikaÖdev/hafta.1/TersÜçgen.java
+++ PatikaÖdev/hafta.1/TersÜçgen.java
@@ -1,4 +1,24 @@
 package PACKAGE_NAME;
 
 public class TersÜçgen {
+   public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Kaç satır olsun :");
+        int number = input.nextInt();
+
+        for (int i = 1; i <= number; i++) {
+            for (int j = 1; j <= number; j++) {
+                System.out.print(" ");
+
+        }
+            int starCount = (number + 1 ) - i ;
+            for(int k = 1 ; k <= 2 * starCount - 1 ; k++) {
+                System.out.print( " * ");
+            }
+            System.out.println();
+
+
+        }
+    }
+  
 }
