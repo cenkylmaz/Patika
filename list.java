95d3b50b693b89563228b336fdf64046eb1139e8
Cenkylmaz <151631495+cenkylmaz@users.noreply.github.com>
2025-03-24 13:47:20 +0300

    MERHABA COMMİT

--- list.java
+++ list.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class list {
+    public static void main(String[] args) {
+        List list = new List();
+
+        List<String> names = new ArrayList<>();
+
+        names.add("Mehmet");
+        names.add("Kaya");
+        names.add("Sayi");
+        names.add("Sayi");
+
+        names.remove("Sayi");
+        int[] sayilara = {1,2,4,50};
+        System.out.println(sayilara[1]);
+    }
 }
