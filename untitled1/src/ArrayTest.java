ab46138923f58beadfac4f6748f8755226ac8aa8
Cenkylmaz <151631495+cenkylmaz@users.noreply.github.com>
2025-03-24 15:03:24 +0300

    yeni commit

--- untitled1/src/ArrayTest.java
+++ untitled1/src/ArrayTest.java
@@ -1,11 +1,10 @@
+import java.util.Scanner;
+
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] sayilara = {1,2,4,50};
-        System.out.println(sayilara[1]); // dizilere bakmak için kapalı parantez kullanıyoruz . ([])
-        System.out.println(sayilara[2]);
-     int[] // array tanımı
-        int[] numbers = {1,2,3,4,5};
-      int [][] // multi arrays
+        Scanner scanner = new Scanner(System.in);
+     int[] ll = new int [10 ];
+        System.out.println(ll.length);
 
 
 
