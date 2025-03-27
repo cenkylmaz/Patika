252f037d31c1f38a2aae672aa2e40d6c5df73a6e
Cenkylmaz <151631495+cenkylmaz@users.noreply.github.com>
2025-03-27 11:55:30 +0300

    çöl commit

---
+++ JAVA102/src/week4/Araba.java
@@ -0,0 +1,12 @@
+package week4;
+
+public class Araba {
+   String renk;
+
+   void calistir (){
+       System.out.println("Araba çalıştır!");
+
+   }
+}
+Araba bmw = new Araba();
+bmw.renk ="Kırmızı"
